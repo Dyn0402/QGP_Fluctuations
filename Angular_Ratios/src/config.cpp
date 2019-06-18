3aa00a3c79bccbb2967e32b009a2b63658e2dbd5
Dyn0402 <Dyn0402@hotmail.com>
2019-06-18 16:58:41 -0700

    Added paths to Angular_Ratios config file

--- Angular_Ratios/src/config.cpp
+++ Angular_Ratios/src/config.cpp
@@ -13,12 +13,12 @@ using namespace std;
 
 
 namespace config {
-	string out_path = "";
-	string in_path = "";
-	string out_root_name = "";
+	string out_path = "/star/u/dneff/Documents/Roli_Thesis/";
+	string in_path = "/gpfs01/star/pwg/dneff/BES1_Trees/dylan_trees/";
+	string out_root_name = "test.root";
 
 
-	vector<string> file_list = {"a","b"};
+	vector<string> file_list = {"auau7","auau11"};
 	vector<int> divs = {2,3,4,5,6};
 
 	string tree_name = "nsmTree";
