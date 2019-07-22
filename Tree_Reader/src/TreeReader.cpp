7cadeb29d7212b94ef2003c0f828a92dee667218
Dyn0402 <Dyn0402@hotmail.com>
2019-07-22 13:12:19 -0700

    Started making TreeReader class.

---
+++ Tree_Reader/src/TreeReader.cpp
@@ -0,0 +1,10 @@
+/*
+ * TreeReader.cpp
+ *
+ *  Created on: Jul 18, 2019
+ *      Author: dylan
+ */
+
+
+
+
---
+++ Tree_Reader/src/TreeReader.h
@@ -0,0 +1,23 @@
+/*
+ * TreeReader.h
+ *
+ *  Created on: Jul 18, 2019
+ *      Author: dylan
+ */
+
+#ifndef TREEREADER_H_
+#define TREEREADER_H_
+
+
+
+class TreeReader {
+public:
+	int test;
+
+private:
+	int test2();
+};
+
+
+
+#endif /* TREEREADER_H_ */
