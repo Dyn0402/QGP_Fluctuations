b080cad30d3e7c3e9db99e9ea28c361958769c08
Dyn0402 <Dyn0402@hotmail.com>
2019-07-25 23:32:37 -0700

    Fleshed out Simulator class with most functionality. Untested.

--- Coalescence_Sim/src/Simulator.cpp
+++ Coalescence_Sim/src/Simulator.cpp
@@ -7,14 +7,137 @@
 
 
 #include <vector>
+#include <cmath>
+
+#include <TRandom3.h>
+
+#include "Simulator.h"
+#include "ratio_methods.h"
 
 using namespace std;
 
 
-class Simulator {
-public:
-	int test;
+Simulator::Simulator() {
+	rand = new TRandom3(0);
+	proton_dist_hist = new TH1D();
+}
 
-private:
-	int test2;
-};
+// Getters
+int Simulator::get_n_events() {
+	return(pars.n_events);
+}
+
+int Simulator::get_divisions() {
+	return(pars.divisions);
+}
+
+double Simulator::get_p_effect() {
+	return(pars.p_effect);
+}
+
+double Simulator::get_p_cluster() {
+	return(pars.p_cluster);
+}
+
+double Simulator::get_spread_sigma() {
+	return(pars.spread_sigma);
+}
+
+
+// Setters
+void Simulator::set_n_events(int n) {
+	pars.n_events = n;
+}
+
+void Simulator::set_divisions(int divs) {
+	pars.divisions = divs;
+}
+
+void Simulator::set_p_effect(double p_eff) {
+	pars.p_effect = p_eff;
+}
+
+void Simulator::set_p_cluster(double p_clust) {
+	pars.p_cluster = p_clust;
+}
+
+void Simulator::set_spread_sigma(double sig) {
+	pars.spread_sigma = sig;
+}
+
+void Simulator::set_min_protons(int protons) {
+	pars.min_protons = protons;
+}
+
+void Simulator::set_proton_dist(string dist) {
+	pars.proton_dist = dist;
+}
+
+void Simulator::set_particle_mean(double mean) {
+	pars.particle_mean = mean;
+}
+
+void Simulator::set_proton_dist_hist(TH1D *hist) {
+	proton_dist_hist = hist;
+	pars.proton_dist = "hist";
+}
+
+
+// Doers
+tree_data Simulator::run_simulation() {
+	vector<double> proton_angles;
+	tree_data data;
+	for(int i = 0; i < pars.n_events; i++) {
+		proton_angles = simulate_event();
+		if(proton_angles.size() < (unsigned)pars.min_protons) { continue; }
+		data.good_protons[0][(int)proton_angles.size()] ++;
+		proton_angles = rotate_angles(proton_angles, rand->Rndm() * 2*M_PI);
+		for(int r:get_Rs(proton_angles, pars.divisions)) {
+			data.ratios[pars.divisions][0][(int)proton_angles.size()][r] ++;
+		}
+	}
+
+	delete rand;
+
+	return(data);
+}
+
+
+vector<double> Simulator::simulate_event() {
+	int n_protons, rand_protons, group_protons;
+	double group_angle;
+	vector<double> proton_angles = {};
+
+	n_protons = get_protons();
+	if(rand->Rndm() < pars.p_effect) {
+		group_protons = (int) (n_protons * pars.p_cluster + 0.5);
+		group_angle = rand->Rndm() * 2 * M_PI;
+		for(int j = 0; j < group_protons; j++) {
+			double cor_angle = rand->Gaus(group_angle, pars.spread_sigma);
+			while(cor_angle > 2*M_PI) { cor_angle -= 2*M_PI; }
+			while(cor_angle < 0) { cor_angle += 2*M_PI; }
+			proton_angles.push_back(cor_angle);
+		}
+	} else {
+		group_protons = 0;
+	}
+
+	rand_protons = n_protons - group_protons;
+	for(int j = 0; j < rand_protons; j++) {
+		proton_angles.push_back(rand->Rndm() * 2 * M_PI);
+	}
+
+	return(proton_angles);
+}
+
+
+int Simulator::get_protons() {
+	int n;
+	if(pars.proton_dist == "poisson") {
+		n = rand->Poisson(pars.particle_mean);
+	} else if(pars.proton_dist == "hist") {
+		n = proton_dist_hist->GetRandom();
+	}
+
+	return (n);
+}
--- Coalescence_Sim/src/Simulator.h
+++ Coalescence_Sim/src/Simulator.h
@@ -12,6 +12,11 @@
 #include <vector>
 #include <string>
 
+#include <TH1.h>
+#include <TRandom3.h>
+
+#include <ratio_methods.h>
+
 using namespace std;
 
 
@@ -19,12 +24,14 @@ struct simulation_pars {
 		int n_events = 1e6;
 		int divisions = 6;
 
-		double particle_mean = 20.0;
 		double p_effect = 0.2;
 		double p_cluster = 0.5;
-
 		double spread_sigma = 0.0;
+
 		int min_protons = 2;
+
+		string proton_dist = "poisson";
+		double particle_mean = 20.0;
 	};
 
 
@@ -32,19 +39,45 @@ class Simulator {
 public:
 	Simulator();
 
-	simulation_pars pars;
+	tree_data run_simulation();
+
+	// Getters
+	int get_n_events();
+	int get_divisions();
+	double get_p_effect();
+	double get_p_cluster();
+	double get_spread_sigma();
+
+	// Setters
+	void set_n_events(int n);
+	void set_divisions(int divs);
+	void set_p_effect(double p_eff);
+	void set_p_cluster(double p_clust);
+	void set_spread_sigma(double sig);
+	void set_min_protons(int protons);
+	void set_proton_dist(string dist);
+	void set_particle_mean(double mean);
+	void set_proton_dist_hist(TH1D *hist);
 
 private:
-	string out_path = "/home/dylan/local_server/dyn0402/Research/Simulation/07-18_athic_comp.root";
+//	string out_path = "/home/dylan/local_server/dyn0402/Research/Simulation/07-18_athic_comp.root";
 	//	string out_path = "/home/dylan/Research/Roli_Sim_Data/comp_test_dylan.root";
 
-	string param = "p_cluster";
-	vector<double> param_list = {0.0, 0.05, 0.10, 0.15, 0.20};
-	vector<int> cumulant_orders = {1, 2, 3, 4};
-	vector<string> statistics = {"mean", "standard_deviation", "skewness", "kurtosis"};
+//	string param = "p_cluster";
+//	vector<double> param_list = {0.0, 0.05, 0.10, 0.15, 0.20};
+//	vector<int> cumulant_orders = {1, 2, 3, 4};
+//	vector<string> statistics = {"mean", "standard_deviation", "skewness", "kurtosis"};
+//
+//	double can_w = 1080;
+//	double can_h = 1080;
 
-	double can_w = 1080;
-	double can_h = 1080;
+	simulation_pars pars;
+	TRandom3 *rand;
+	TH1D *proton_dist_hist;
+
+	vector<double> Simulator::simulate_event();
+	int get_protons();
+
 };
 
 
