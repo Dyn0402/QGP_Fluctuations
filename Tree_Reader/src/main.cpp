ce220fab2874a1cf27182526ef812250e5d0bb51
Dyn0402 <Dyn0402@hotmail.com>
2019-11-28 00:15:38 -0800

    Set up main in Tree_Reader for paper run. Implemented ThreadPool.
    Planning to use this commit to produce paper results.

--- Tree_Reader/src/main.cpp
+++ Tree_Reader/src/main.cpp
@@ -23,6 +23,7 @@
 #include <TROOT.h>
 #include <TCanvas.h>
 
+#include "ThreadPool.h"
 #include "TreeReader.h"
 #include "ratio_methods.h"
 #include "file_io.h"
@@ -53,52 +54,58 @@ void read_class() {
 	string mix_sets_out_dir = "/home/dylan/Research/Data_Mix_Sets/";
 	string mix_out_dir = "/home/dylan/Research/Data_Mix/";
 	string random_out_dir = "/home/dylan/Research/Data_Random/";
-	string set_name = "test";
-	int set_num_min = 0, set_num_max = 5;
 
-	vector<string> set_dirs;
-	for(int set = set_num_min; set <= set_num_max; set++) {
-		set_dirs.push_back(set_name + to_string(set) + "/");
-	}
+	map<string, int> set_pairs = {{"No_Rotate",10}, {"Rand_Rotate",25}, {"EP_Rotate",10}, {"Pile_Up_05_",10}, {"Efficiency_05_",10}};
 
-	for(string set:set_dirs) {
-		cout << endl << endl << "Starting " + set << endl << endl;
-		int set_num = stoi(set.substr(set.find(set_name.back())+1, set.find('/')-set.find(set_name.back())-1));
-		vector<int> energy_list = {27, 39, 62, 19, 11, 7};
-		vector<int> divs = {2, 3, 4, 5, 6};
-		ROOT::EnableThreadSafety();
-		vector<thread> threads;
-		for(int energy:energy_list) {
-			TreeReader reader(energy);
-			reader.set_cbwc(false);
-			reader.set_cent_binning(9);
-			reader.set_in_path("/home/dylan/Research/Trees/");
-			reader.set_out_path(out_dir+set);
-			reader.set_qa_path(out_dir+set+to_string(energy)+"GeV/");
-			reader.set_qa_name("QA_");
-			reader.set_divs(divs);
-			if(set_num < 6) { reader.set_event_plane(true); }
-			else { reader.set_event_plane(false); }
-			if(set_num >= 6 && set_num < 12) { reader.set_rotate_random(true); }
-			else { reader.set_rotate_random(false); }
-			reader.set_mixed_sets(false);
+	int sleep = 60;
 
-			reader.set_mixed(true);
-			reader.mix.set_divs(divs);
-			reader.mix.set_out_path(mix_out_dir+set);
-			reader.mix.set_max_events(250);
-			reader.mix.set_min_events(150);
-			if(energy <= 11) { reader.mix.set_mixes_per_event(50); }
-			else { reader.mix.set_mixes_per_event(10); }
-			reader.set_rand_data(false);
-			reader.random.set_divs(divs);
-			reader.random.set_out_path(random_out_dir+set+to_string(energy)+"GeV/");
-			threads.push_back(thread(&TreeReader::read_trees, reader));
-		}
-		for(thread & th : threads) {
-			if(th.joinable()) {
-				th.join();
+	ROOT::EnableThreadSafety();
+	{
+		ThreadPool pool(thread::hardware_concurrency());
+		for(pair<string, int> set_pair:set_pairs) {
+			for(int set_num = 0; set_num < set_pair.second; set_num++) {
+				string set_dir = set_pair.first + to_string(set_num) + "/";
+				cout << endl << endl << "Queueing " + set_dir <<  "  set_num: " << set_num << endl << endl;
+				vector<int> energy_list = {39, 27, 62, 19, 11, 7};
+				vector<int> divs = {2, 3, 4, 5, 6};
+				for(int energy:energy_list) {
+					TreeReader reader(energy);
+					reader.set_divs(divs);
+					reader.set_cbwc(false);
+					reader.set_cent_binning(9);
+
+					reader.set_in_path("/home/dylan/Research/Trees/");
+					reader.set_out_path(out_dir+set_dir);
+					reader.set_qa_path(out_dir+set_dir+to_string(energy)+"GeV/");
+					reader.set_qa_name("QA_");
+					reader.set_set_name(set_pair.first + to_string(set_num));
+
+					if(set_pair.first == "EP_Rotate")  { reader.set_event_plane(true); }
+					else { reader.set_event_plane(true); }
+
+					if(set_pair.first == "No_Rotate" || set_pair.first == "EP_Rotate") { reader.set_rotate_random(true); }
+					else{ reader.set_rotate_random(false); }
+
+					if(set_pair.first == "Pile_Up_05_") { reader.set_pile_up(true); reader.set_pile_up_prob(0.05); }
+					else { reader.set_pile_up(false); reader.set_pile_up_prob(0); }
+
+					if(set_pair.first == "Efficiency_05_") { reader.set_efficiency(true); reader.set_efficiency_prob(0.05); }
+					else { reader.set_efficiency(false); reader.set_efficiency_prob(0); }
+
+					reader.set_mixed_sets(false);
+					reader.set_rand_data(false);
+
+					reader.set_mixed(true);
+					reader.mix.set_divs(divs);
+					reader.mix.set_out_path(mix_out_dir+set_dir);
+					reader.mix.set_max_events(250);
+					reader.mix.set_min_events(150);
+					if(energy <= 11) { reader.mix.set_mixes_per_event(50); }
+					else { reader.mix.set_mixes_per_event(10); }
+					pool.enqueue(&TreeReader::read_trees, reader);
+				}
 			}
+			this_thread::sleep_for(chrono::seconds(sleep));
 		}
 	}
 }
