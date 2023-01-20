11970dfbca4f333d86ef03281f8150c9f539a5c8
Dyn0402 <40580528+Dyn0402@users.noreply.github.com>
2023-01-19 18:47:04 -0800

    Tree_Reader set to test flow to see if it's working right

--- Tree_Reader/src/main.cpp
+++ Tree_Reader/src/main.cpp
@@ -215,25 +215,25 @@ void read_new() {
 //		{"BES1_Eff15_sys_2", {{"Eff15_sys", get_rand_set_pairs(10, "rapid05", "Efficiency15")}}}
 //	};
 
-//	map<string, map<string, map<string, pair<int, int>>>> sets = {
-//		{"Sim_flow_flat80_res08_v205_test", {{"flow_flat80_res08_v205_resample_test", {{"Sim_Flow_flat80_res08_v205_resample_", {0, 0}}}}}},
-//		{"Sim_flow_flat80_res9_v204_test", {{"flow_flat80_flow_res9_v204_resample_test", {{"Sim_Flow_flat80_res9_v204_resample_", {0, 0}}}}}},
-//	};
-
 	map<string, map<string, map<string, pair<int, int>>>> sets = {
-		{"Sim_Flow_flat80_res99_v207", {{"flow_flat80_res99_v207_resample", {{"Sim_Flow_flat80_res99_v207_resample_norotate_", {0, 0}}}}}},
-		{"Sim_Flow_flat80_res9_v207", {{"flow_flat80_res9_v207_resample", {{"Sim_Flow_flat80_res9_v207_resample_norotate_", {0, 0}}}}}},
-		{"Sim_Flow_flat80_res75_v207", {{"flow_flat80_res75_v207_resample", {{"Sim_Flow_flat80_res75_v207_resample_norotate_", {0, 0}}}}}},
-		{"Sim_Flow_flat80_res6_v207", {{"flow_flat80_res6_v207_resample", {{"Sim_Flow_flat80_res6_v207_resample_norotate_", {0, 0}}}}}},
-		{"Sim_Flow_flat80_res5_v207", {{"flow_flat80_res5_v207_resample", {{"Sim_Flow_flat80_res5_v207_resample_norotate_", {0, 0}}}}}},
-		{"Sim_Flow_flat80_res4_v207", {{"flow_flat80_res4_v207_resample", {{"Sim_Flow_flat80_res4_v207_resample_norotate_", {0, 0}}}}}},
-		{"Sim_Flow_flat80_res3_v207", {{"flow_flat80_res3_v207_resample", {{"Sim_Flow_flat80_res3_v207_resample_norotate_", {0, 0}}}}}},
-		{"Sim_Flow_flat80_res2_v207", {{"flow_flat80_res2_v207_resample", {{"Sim_Flow_flat80_res2_v207_resample_norotate_", {0, 0}}}}}},
-		{"Sim_Flow_flat80_res15_v207", {{"flow_flat80_res15_v207_resample", {{"Sim_Flow_flat80_res15_v207_resample_norotate_", {0, 0}}}}}},
-		{"Sim_Flow_flat80_res1_v207", {{"flow_flat80_res1_v207_resample", {{"Sim_Flow_flat80_res1_v207_resample_norotate_", {0, 0}}}}}},
-		{"Sim_Flow_flat80_res15_v205", {{"flow_flat80_res15_v205_resample", {{"Sim_Flow_flat80_res15_v205_resample_norotate_", {0, 0}}}}}},
-		{"Sim_Flow_flat80_res15_v202", {{"flow_flat80_res15_v202_resample", {{"Sim_Flow_flat80_res15_v202_resample_norotate_", {0, 0}}}}}},
+		//{"Sim_flow_flat80_res08_v205_test", {{"flow_flat80_res08_v205_resample_test", {{"Sim_Flow_flat80_res08_v205_resample_", {0, 0}}}}}},
+		{"Sim_flow_flat80_res9_v204_test", {{"flow_flat80_flow_res9_v204_resample_test", {{"Sim_Flow_flat80_res9_v204_resample_", {0, 0}}}}}},
 	};
+
+	//map<string, map<string, map<string, pair<int, int>>>> sets = {
+	//	{"Sim_Flow_flat80_res99_v207", {{"flow_flat80_res99_v207_resample", {{"Sim_Flow_flat80_res99_v207_resample_norotate_", {0, 0}}}}}},
+	//	{"Sim_Flow_flat80_res9_v207", {{"flow_flat80_res9_v207_resample", {{"Sim_Flow_flat80_res9_v207_resample_norotate_", {0, 0}}}}}},
+	//	{"Sim_Flow_flat80_res75_v207", {{"flow_flat80_res75_v207_resample", {{"Sim_Flow_flat80_res75_v207_resample_norotate_", {0, 0}}}}}},
+	//	{"Sim_Flow_flat80_res6_v207", {{"flow_flat80_res6_v207_resample", {{"Sim_Flow_flat80_res6_v207_resample_norotate_", {0, 0}}}}}},
+	//	{"Sim_Flow_flat80_res5_v207", {{"flow_flat80_res5_v207_resample", {{"Sim_Flow_flat80_res5_v207_resample_norotate_", {0, 0}}}}}},
+	//	{"Sim_Flow_flat80_res4_v207", {{"flow_flat80_res4_v207_resample", {{"Sim_Flow_flat80_res4_v207_resample_norotate_", {0, 0}}}}}},
+	//	{"Sim_Flow_flat80_res3_v207", {{"flow_flat80_res3_v207_resample", {{"Sim_Flow_flat80_res3_v207_resample_norotate_", {0, 0}}}}}},
+	//	{"Sim_Flow_flat80_res2_v207", {{"flow_flat80_res2_v207_resample", {{"Sim_Flow_flat80_res2_v207_resample_norotate_", {0, 0}}}}}},
+	//	{"Sim_Flow_flat80_res15_v207", {{"flow_flat80_res15_v207_resample", {{"Sim_Flow_flat80_res15_v207_resample_norotate_", {0, 0}}}}}},
+	//	{"Sim_Flow_flat80_res1_v207", {{"flow_flat80_res1_v207_resample", {{"Sim_Flow_flat80_res1_v207_resample_norotate_", {0, 0}}}}}},
+	//	{"Sim_Flow_flat80_res15_v205", {{"flow_flat80_res15_v205_resample", {{"Sim_Flow_flat80_res15_v205_resample_norotate_", {0, 0}}}}}},
+	//	{"Sim_Flow_flat80_res15_v202", {{"flow_flat80_res15_v202_resample", {{"Sim_Flow_flat80_res15_v202_resample_norotate_", {0, 0}}}}}},
+	//};
 
 
 //	map<string, map<string, map<string, pair<int, int>>>> sets = {
